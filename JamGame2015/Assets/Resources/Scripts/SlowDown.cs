378afbd977e3c6dcce16bec42805a24bb890b720
Jared Hill <jhill2@spsu.edu>
2015-07-10 05:50:37 -0400

    Adding Time Slowdown
    Added script that allows time to be slowed down.

---
+++ JamGame2015/Assets/Resources/Scripts/SlowDown.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlowDown : MonoBehaviour {
+	public float slowDownFactor;
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("e")) {
+			if (Time.timeScale == 1) {
+				Time.timeScale = slowDownFactor;
+			}
+			else {
+				Time.timeScale = 1.0f;
+			}
+			Time.fixedDeltaTime = slowDownFactor;
+		}
+	}
+}
