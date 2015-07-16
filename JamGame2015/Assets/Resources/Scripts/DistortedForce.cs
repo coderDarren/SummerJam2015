6707349cd91f5317faae1f726f6d5e5a91ccca53
Darren O'Neale <doneale3@gmail.com>
2015-07-16 17:08:03 -0400

    Added Distortion Power Blast Effect
    just open distortion scene and play

---
+++ JamGame2015/Assets/Resources/Scripts/DistortedForce.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DistortedForce : MonoBehaviour {
+
+    public float expandRate = 0.25f;
+    public Size size;
+
+    [System.Serializable]
+    public struct Size { public float min; public float max;}
+
+    float currScale = 0;
+
+    void Start()
+    {
+        currScale = size.min;
+        transform.localScale = new Vector3(currScale, currScale, currScale);
+    }
+
+    void Update()
+    {
+        currScale += expandRate;
+
+        if (currScale > size.max)
+            currScale = size.min;
+
+        transform.localScale = new Vector3(currScale, currScale, currScale);
+    }
+}
