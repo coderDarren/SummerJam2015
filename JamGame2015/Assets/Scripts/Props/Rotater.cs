df4dfe085bd3861ac4365f750663191e8ac5aace
Darren O'Neale <doneale3@gmail.com>
2015-07-20 07:15:45 -0400

    Level 1 Tweaks
    -made first jump a bit harder
    -added "parentable" option for the Rotater.cs script. When the player
    lands on a blade, the blade will now actually carry them around until
    the player jumps

--- JamGame2015/Assets/Scripts/Props/Rotater.cs
+++ JamGame2015/Assets/Scripts/Props/Rotater.cs
@@ -5,6 +5,7 @@ public class Rotater : MonoBehaviour {
 
     public Vector3 rotateAxis = new Vector3(0, 1, 0);
     public float rotateSmooth = 5f;
+    public bool parentable = false;
 
     float _rotateSmooth = 0;
 
@@ -31,5 +32,23 @@ public class Rotater : MonoBehaviour {
     void AlterSpeed(float speedFactor)
     {
         _rotateSmooth = rotateSmooth * speedFactor;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag.Equals("Player"))
+        {
+            if (parentable)
+                col.transform.parent = transform;
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.tag.Equals("Player"))
+        {
+            if (parentable)
+                col.transform.parent = null;
+        }
     }
 }
