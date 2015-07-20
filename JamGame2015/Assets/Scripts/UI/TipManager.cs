7ee0b89eff2cea9d4d4294c134d3d1ec8acd3d60
Darren O'Neale <doneale3@gmail.com>
2015-07-20 06:46:43 -0400

    Tip Manager Complete

---
+++ JamGame2015/Assets/Scripts/UI/TipManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TipManager : MonoBehaviour {
+
+    public string[] tips;
+    public GameObject TipInterface;
+    public float tipFrequency = 80;
+
+    GameObject tipInterface;
+    Image[] images;
+    Text tip;
+    float frequencyTimer = 0;
+    float colorAlpha = 1;
+    int tipIndex = 0;
+
+    void Start()
+    {
+        SpawnNewTip();
+    }
+
+    void Update()
+    {
+        RunTimers();
+
+        if (frequencyTimer >= tipFrequency)
+        {
+            SpawnNewTip();
+            frequencyTimer = 0;
+        }
+
+        if (tipInterface)
+        {
+            if (tip.color.a <= 0.1f)
+            {
+                Destroy(tipInterface);
+                frequencyTimer = 0;
+                colorAlpha = 1;
+            }
+        }
+    }
+
+    void RunTimers()
+    {
+        frequencyTimer += Time.deltaTime;
+    }
+
+    void SpawnNewTip()
+    {
+        tipInterface = Instantiate(TipInterface as GameObject);
+        tipInterface.transform.parent = transform;
+        images = tipInterface.GetComponentsInChildren<Image>();
+        tip = tipInterface.GetComponentInChildren<Text>();
+        tip.text = tips[tipIndex];
+
+        tipIndex++;
+        if (tipIndex > tips.Length - 1)
+            tipIndex = 0;
+    }
+
+    
+}
