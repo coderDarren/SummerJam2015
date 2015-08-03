f0b21c8e2dc1f89354f9e4d339818c3ee89d84bb
Jared Hill <jhill2@spsu.edu>
2015-08-03 02:23:59 -0400

    Working on level and scripts
    Adding in new simple lerper script and working on level

---
+++ JamGame2015/Assets/Scripts/Props/SimpleLerper.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleLerper : MonoBehaviour {
+
+	public Vector3 pointA, pointB;
+	public bool parentable;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		pointA = this.transform.position;
+	}
+
+	IEnumerator MoveBlock(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
+	{
+		var i= 0.0f;
+		var rate= 1.0f/time;
+		while (i < 1.0f) {
+			i += Time.deltaTime * rate;
+			thisTransform.position = Vector3.Lerp(startPos, endPos, i);
+			yield return null; 
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position == pointA)
+		{
+			StartCoroutine(MoveBlock(transform, pointA, pointB, speed));
+		}
+		else if (transform.position == pointB)
+		{
+			StartCoroutine(MoveBlock(transform, pointB, pointA, speed));
+		}
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag.Equals("Player"))
+		{
+			if (parentable)
+				col.transform.parent = transform;
+		}
+	}
+	
+	void OnCollisionExit(Collision col)
+	{
+		if (col.gameObject.tag.Equals("Player"))
+		{
+			if (parentable)
+				col.transform.parent = null;
+		}
+	}
+}
